ff8b172b7016c924e71fef8ec535e62541d035d3
tdebug163 <twqyqmhmd95@gmail.com>
2026-05-29 01:33:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ except BaseException as e:
 # --- المتغيرات ---
 API_ID = 28797361  
 API_HASH = "771041b32e83ab232e066b7adeee700b"  
-BOT_TOKEN = "8971197244:AAHRk4mTQ1ifMQ_lpIkA5ncQF2S2y6yWiwU"  
+BOT_TOKEN = "8971197244:AAEekSAIRnhlAqvqIKc6T8F3m3o5JyFSooM"  
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
